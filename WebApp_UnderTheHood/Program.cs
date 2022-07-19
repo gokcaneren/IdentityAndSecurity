b2437b36fc7c043512a7cf4fb8fab7ac591850f7
gokcaneren <gokcann01@gmail.com>
2022-07-19 14:38:40 +0300

    requirement added

--- WebApp_UnderTheHood/Program.cs
+++ WebApp_UnderTheHood/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
+using WebApp_UnderTheHood.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,8 +22,11 @@ builder.Services.AddAuthorization(opt =>
 
     opt.AddPolicy("HRManagerOnly",
         policy => policy.RequireClaim("Department", "HR")
-        .RequireClaim("Manager"));
+        .RequireClaim("Manager")
+        .Requirements.Add(new HRManagerProbationRequirement(3)));
 });
+
+builder.Services.AddSingleton<IAuthorizationHandler, HRManagerProbationRequirementHandler>();
 
 var app = builder.Build();
 
