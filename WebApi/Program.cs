d217bc7c1c2d6c130a0e331b1dacfc4aaa75c70e
gokcaneren <gokcann01@gmail.com>
2022-07-20 16:53:38 +0300

    api and app connection completed

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,6 +7,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt => opt.AddPolicy("allow", x =>
+{
+    x.AllowAnyOrigin();
+    x.AllowAnyHeader();
+    x.AllowAnyMethod();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- WebApp_UnderTheHood/Pages/HrManager.cshtml.cs
+++ WebApp_UnderTheHood/Pages/HrManager.cshtml.cs
@@ -1,14 +1,25 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp_UnderTheHood.DTO;
 
 namespace WebApp_UnderTheHood.Pages
 {
     [Authorize(Policy = "HRManagerOnly")]
     public class HRManagerModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory httpClientFactory;
+        [BindProperty]
+        public List<WeatherForecastDTO> WeatherForecastItems { get; set; }
+        public HRManagerModel(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task OnGet()
+        {
+            var httpClient = httpClientFactory.CreateClient("OurWebApi");
+            WeatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast");
         }
     }
 }
